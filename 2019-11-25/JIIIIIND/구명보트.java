f99f9df30b9285aa7c0c571060b242f2e6ea711a
JIND <jinwoo700@gmail.com>
2019-12-01 17:35:22 +0900

    구명보트 문제 수정
    -기존 효율성 문제 해결

--- 2019-11-25/JIIIIIND/구명보트.java
+++ 2019-11-25/JIIIIIND/구명보트.java
@@ -1,47 +1,29 @@
-import java.util.Collections;
-import java.util.ArrayList;
-
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit)
     {
         int answer = 0;
-        ArrayList<Integer> boatPeople = new ArrayList<>();
-        for(int value : people)
-            boatPeople.add(value);
-        //오름차순 정렬 됨
-        Collections.sort(boatPeople);
 
-        while(!boatPeople.isEmpty())
+        Arrays.sort(people);
+
+        int little = 0;
+        int large = people.length-1;
+
+        while(little <= large)
         {
-            int first = boatPeople.get(boatPeople.size()-1);
-            int nextIndex = -1;
-            for(int i = 0; i < boatPeople.size()-1; i++)
+            if((people[large] + people[little]) > limit)
             {
-                if(boatPeople.get(i) + first <= limit)
-                    nextIndex = i;
-                else
-                {
-                    if(nextIndex >= 0)
-                    {
-                        boatPeople.remove(nextIndex);
-                        nextIndex = -1;
-                    }
-                    break;
-                }
-            }
-            if(nextIndex >= 0)
-            {
-                boatPeople.remove(nextIndex);
-                boatPeople.remove(boatPeople.size()-1);
-                answer++;
+                large--;
             }
             else
             {
-                boatPeople.remove(boatPeople.size()-1);
-                answer++;
+                little++;
+                large--;
             }
+            answer++;
         }
+
         return answer;
     }
 }
